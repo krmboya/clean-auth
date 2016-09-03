bb93cbd65ea85197ac3264b819e51af2dc54b177
Kenny Rachuonyo <krmboya@gmail.com>
2016-09-03 12:17:36 +0300

    modify schema

--- schema.sql
+++ schema.sql
@@ -1,8 +1,10 @@
 create table user (
        id serial not null,
-       username text not null,
+       email text not null,
        password text not null,
-       email text, -- optional
+       username text, -- optional
+       first_name text, -- optional
+       last_name text, -- optional
        status text not null default 'inactive',
        last_login timestamptz not null default now(), -- utc
        date_joined timestamptz not null default now() -- utc
